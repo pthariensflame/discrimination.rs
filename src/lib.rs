41df4014d91f0d337c3923ee2e68a0e21e898ed4
Alexander Ronald Altman <alexanderaltman@me.com>
2016-12-14 15:31:28 -0800

    Add comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
+//! Generic worst-case-linear-time sorting and partitioning algorithms based on [discrim](http://www.diku.dk/hjemmesider/ansatte/henglein/papers/henglein2011a.pdf)[inators](http://www.diku.dk/hjemmesider/ansatte/henglein/papers/henglein2011c.pdf).  Partially a port of the [Haskell library of the same name](https://github.com/ekmett/discrimination).
+
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-    }
+    fn it_works() {}
 }

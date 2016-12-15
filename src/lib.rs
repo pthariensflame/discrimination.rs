667a862b51a7c0f240363a049c4d03844a7e6a80
Alexander Ronald Altman <alexanderaltman@me.com>
2016-12-14 16:22:17 -0800

    Stuff

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,9 @@
 //! [discrim][1][inators][2].  Partially a port of the [Haskell library of the
 //! same name][3].
 //!
-//! [1]: http://www.diku.dk/hjemmesider/ansatte/henglein/papers/henglein2011a.
-//! pdf
-//! [2]: http://www.diku.dk/hjemmesider/ansatte/henglein/papers/henglein2011c.
-//! pdf
-//! [3]: https://github.com/ekmett/discrimination
+//! [1]:http://www.diku.dk/hjemmesider/ansatte/henglein/papers/henglein2011a.pdf
+//! [2]:http://www.diku.dk/hjemmesider/ansatte/henglein/papers/henglein2011c.pdf
+//! [3]:https://github.com/ekmett/discrimination
 
 #[cfg(test)]
 mod tests {
